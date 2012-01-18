35f1650771c71d6be1581dd87b2e8e5a54c8371f
Hidden <zzdhidden@gmail.com>
2012-01-18 20:41:25 +0800

    2000ms login timeout

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -31,7 +31,7 @@
 
 // ==UserScript==  
 // @name         12306 Booking Assistant
-// @version		 1.3.9
+// @version		 1.3.10
 // @author       zzdhidden@gmail.com
 // @namespace    https://github.com/zzdhidden
 // @description  12306 订票助手之(自动登录，自动查票，自动订单)
